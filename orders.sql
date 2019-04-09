d7503144511d37db9b7ac696ed6ff8ab1ca7cee4
EricCartman598 <47695625+EricCartman598@users.noreply.github.com>
2019-04-09 15:19:58 +0300

    Delete orders.sql

--- orders.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS orders;
-CREATE TABLE orders
-(
-	id 					INT PRIMARY KEY AUTO_INCREMENT,
-	userId 				INT NOT NULL,
-	price				DOUBLE,
-	countriesID			ARRAY,	
-	FOREIGN KEY(userId) REFERENCES users(id)
-);   
